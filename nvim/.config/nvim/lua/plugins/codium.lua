0d5f1e33d0cb3c85dfb6aaa1e61b463d0b7f3ac1
Fayaz <mdfayaz9963@gmail.com>
2024-11-12 06:46:11 +0000

    update to neocodium

--- nvim/.config/nvim/lua/plugins/codium.lua
+++ nvim/.config/nvim/lua/plugins/codium.lua
@@ -1,4 +1,10 @@
 return {
-  'Exafunction/codeium.vim',
-  event = 'BufEnter'
+  "monkoose/neocodeium",
+  event = "VeryLazy",
+  config = function()
+    local neocodeium = require("neocodeium")
+    neocodeium.setup()
+    vim.keymap.set("i", "<A-f>", neocodeium.accept)
+  end,
 }
+
