292f4cdf2341a9ff005a6158bc00d8c068378e4a
Fayaz Mohammad <mdfayaz9963@gmail.com>
2025-04-12 03:25:13 +0530

    added image in nvim

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -10,21 +10,8 @@ return {
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
-    statuscolumn = {
-      enabled = true,
-      left = { "mark", "sign" }, -- priority of signs on the left (high to low)
-      right = { "fold", "git" }, -- priority of signs on the right (high to low)
-      folds = {
-        open = false,            -- show open fold icons
-        git_hl = false,          -- use Git Signs hl for fold icons
-      },
-      git = {
-        -- patterns to match Git signs
-        patterns = { "GitSign", "MiniDiffSign" },
-      },
-      refresh = 50, -- refresh at most every 50ms
-    },
     words = { enabled = true },
+    image = { enabled = true },
     toggle = {
       map = vim.keymap.set, -- keymap.set function to use
       which_key = true,     -- integrate with which-key to show enabled/disabled icons and colors
