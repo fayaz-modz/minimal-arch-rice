68ab356c28cf62a3471a5b5703b2c6eb3e38bab1
Fayaz Mohammad <mdfayaz9963@gmail.com>
2025-05-12 23:36:33 +0530

    updated neovim

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ui = {
@@ -10,7 +21,7 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = {}
+    config = false
   },
   {
     "neovim/nvim-lspconfig",
@@ -18,36 +29,20 @@ return {
       "saghen/blink.cmp"
     },
     config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      require('mason-lspconfig').setup_handlers({
-        function(server)
-          lspconfig[server].setup({capabilities = capabilities})
-        end,
-      })
-      local keymap = vim.keymap.set
-      keymap('n', 'K', vim.lsp.buf.hover, { desc = "Show hover information" })
-      keymap('n', '<leader>gd', vim.lsp.buf.definition, { desc = "Go to definition" })
-      keymap('n', '<leader>gr', vim.lsp.buf.references, { desc = "Go to references" })
-      keymap('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Trigger code action" })
-      keymap('n', '<leader>gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
-      keymap('n', '<leader>gi', vim.lsp.buf.implementation, { desc = "Go to implementation" })
-      keymap('n', '<C-k>', vim.lsp.buf.signature_help, { desc = "Show function signature" })
-      keymap('n', '<leader>D', vim.lsp.buf.type_definition, { desc = "Go to type definition" })
-      keymap('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Rename symbol" })
-      keymap('n', 'gr', vim.lsp.buf.references, { desc = "Go to references (alternative)" })
-      keymap('n', '<leader>k', vim.diagnostic.open_float, { desc = "Open floating diagnostic" })
+      require("mason-lspconfig").setup()
     end,
-  },
-  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
+    keymaps = {
+      {'K', vim.lsp.buf.hover, { desc = "Show hover information" }},
+      {'<leader>gd', vim.lsp.buf.definition, { desc = "Go to definition" }},
+      {'<leader>gr', vim.lsp.buf.references, { desc = "Go to references" }},
+      {'<leader>ca', vim.lsp.buf.code_action, { desc = "Trigger code action" }},
+      {'<leader>gD', vim.lsp.buf.declaration, { desc = "Go to declaration" }},
+      {'<leader>gi', vim.lsp.buf.implementation, { desc = "Go to implementation" }},
+      {'<C-k>', vim.lsp.buf.signature_help, { desc = "Show function signature" }},
+      {'<leader>D', vim.lsp.buf.type_definition, { desc = "Go to type definition" }},
+      {'<leader>rn', vim.lsp.buf.rename, { desc = "Rename symbol" }},
+      {'gr', vim.lsp.buf.references, { desc = "Go to references (alternative)" }},
+      {'<leader>k', vim.diagnostic.open_float, { desc = "Open floating diagnostic" }},
     },
   },
 }
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -33,8 +33,51 @@ return {
     }
   },
   keys = {
-    { "<leader>z", function() Snacks.zen() end,           desc = "zen mode" },
-    { "<leader>h", function() Snacks.notifier.hide() end, desc = "zen mode" },
-    { "<leader>/", function() Snacks.terminal() end,      desc = "terminal toggle" },
+    { "<leader>d", function() Snacks.dashboard() end,             desc = "dashboard toggle" },
+    { "<leader>z", function() Snacks.zen() end,                   desc = "zen mode" },
+    { "<leader>h", function() Snacks.notifier.hide() end,         desc = "zen mode" },
+    { "<leader>H", function() Snacks.notifier.show_history() end, desc = "zen mode" },
+    { "<leader>/", function() Snacks.terminal() end,              desc = "terminal toggle" },
+    { "<leader>E", function() Snacks.explorer() end,              desc = "file explorer" },
+    {
+      "<leader>e",
+      function()
+        Snacks.explorer({
+          auto_close = true,
+          layout = {
+            layout = {
+              backdrop = false,
+              row = 1,
+              width = 0.4,
+              min_width = 80,
+              height = 0.8,
+              border = "none",
+              box = "vertical",
+              { win = "preview", title = "{preview}", height = 0.4, border = "rounded" },
+              {
+                box = "vertical",
+                border = "rounded",
+                title = "{title} {live} {flags}",
+                title_pos = "center",
+                { win = "input", height = 1,     border = "bottom" },
+                { win = "list",  border = "none" },
+              },
+            },
+          }
+        })
+      end,
+      desc = "file explorer"
+    },
+    { "<C-p>f",           function() Snacks.picker.files() end,                 { desc = "Files" } },
+    { "<leader><leader>", function() Snacks.picker.recent() end,                { desc = "Recent files" } },
+    { '<C-p>gp',          function() Snacks.picker.grep() end,                  { desc = "Grep" } },
+    { '<C-p>b',           function() Snacks.picker.buffers() end,               { desc = "Buffers" } },
+    { '<C-p>h',           function() Snacks.picker.help() end,                  { desc = "Help" } },
+    { '<C-p>gf',          function() Snacks.picker.git_files() end,             { desc = "Git files" } },
+    { '<C-p>d',           function() Snacks.picker.diagnostics() end,           { desc = "Diagnostics" } },
+    { '<C-p>cm',          function() Snacks.picker.commands() end,              { desc = "Commands" } },
+    { '<C-p>s',           function() Snacks.picker.lsp_symbols() end,           { desc = "Doc symbols" } },
+    { '<C-p>w',           function() Snacks.picker.lsp_workspace_symbols() end, { desc = "WS symbols" } },
+    { '<C-p>r',           function() Snacks.picker.lsp_references() end,        { desc = "References" } },
   },
 }
